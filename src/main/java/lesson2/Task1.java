2a25b57da1597c2f1ca75fc6232c67015e7040ba
Ross <Admin>
2020-05-04 07:42:02 +0300

    Completed Task 1 of Lesson 2

--- src/main/java/lesson2/Task1.java
+++ src/main/java/lesson2/Task1.java
@@ -1,5 +1,7 @@
 package lesson2;
 
+import java.sql.SQLOutput;
+
 /**
  * Реализовать у класса Task1 переменные age (тип int),  name (тип String), ageGroup(тип String)
  * реализовать методы геттеры и сеттеры для каждой переменной.
@@ -20,5 +22,90 @@ package lesson2;
  */
 
 public class Task1 {
+
+    public static void main(String[] args) {
+
+        Task1 Ross = new Task1(11,"Ross","Child");
+
+        Ross.setAge(150);
+        System.out.println(Ross.getAge());
+        Ross.setAge(-20);
+        System.out.println(Ross.getAge());
+        Ross.setName("12");
+        System.out.println(Ross.getName());
+        Ross.setName("   ");
+        System.out.println(Ross.getName());
+        Ross.setName("rostylav");
+        System.out.println(Ross.getName());
+        Ross.setAge(55);
+        System.out.println(Ross.getAgeGroup());
+        Ross.setAgeGroup("NULL");
+        System.out.println(Ross.getAgeGroup());
+
+    }
+
+        private static int age;
+        private static String name;
+        private String ageGroup;
+
+        public Task1(int age,String name, String ageGroup){
+            this.age = age;
+            this.name = name;
+            this.ageGroup = ageGroupSelector();
+        }
+
+    private String ageGroupSelector() {  //create a method - selector of the correct age group.
+            if (this.getAge()<15){
+                ageGroup="child";
+            }
+            else if (this.getAge()>14 && this.getAge()<26){
+               ageGroup="student";
+            }
+            else if (this.getAge()>25 && this.getAge()<66){
+                ageGroup="worker";
+            }
+            else if(this.getAge()>=66){
+                ageGroup="pensioner";
+            }
+        return ageGroup;
+    }
+
+    // Getters
+    public int getAge () {
+        return age;
+    }
+    public String getName () {
+        return name;
+    }
+    public String getAgeGroup () {
+        return ageGroup;
+    }
+    //Setters
+    public void setAge ( int age){
+        if ((age >= 0) && (age <= 100)){
+            this.age = age;
+            this.ageGroupSelector();// - if ageGroup setter was called, I'm also calling my Group selector.
+        }
+        else {
+            System.out.println("Age should be bigger then 0 and less then 100");
+        }
+    }
+    public void setName (String name){
+            if (name.length() < 3 || name.length() >50){
+                System.out.println("Name should be at least 3 and maximum 50 chars long");
+            }
+            else if (name.trim().length()==0){
+                System.out.println("Name should not consist only of spaces");
+            }
+            else{
+                name = name.substring(0, 1).toUpperCase() + name.substring(1);
+                this.name = name;
+            }
+
+    }
+    private void setAgeGroup (String ageGroup) {
+        this.ageGroup = ageGroup;
+    }
+
 
 }
